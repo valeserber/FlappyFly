00a987efaadbd8d99757696974e614d48ac9ef38
Meli Rossi <merossi@itba.edu.ar>
2015-05-22 20:15:03 -0300

    Cambios clase 22/05

---
+++ FlappyFly.spritebuilder/Obstacle.h
@@ -0,0 +1,13 @@
+//
+//  Obstacle.h
+//  FlappyFly
+//
+//  Created by Melisa Anabella Rossi on 22/5/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#ifndef FlappyFly_Obstacle_h
+#define FlappyFly_Obstacle_h
+
+
+#endif
---
+++ FlappyFly.spritebuilder/Obstacle.m
@@ -0,0 +1,9 @@
+//
+//  Obstacle.m
+//  FlappyFly
+//
+//  Created by Melisa Anabella Rossi on 22/5/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
